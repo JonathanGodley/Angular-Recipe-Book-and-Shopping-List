94e555ebaeee396ad95426c0704329387027f7e4
Jonathan <archie88a+gitkraken@gmail.com>
2018-12-21 17:10:20 +1100

    Displayed list of recipes is now generated from saved recipes in the array.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -6,8 +6,15 @@ import {Recipe} from '../recipe.model';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
+// TODO: use local image hosting rather than URLs
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe(
+      'A Test Recipe',
+      'This is a test recipe used for testing the + application\'s basic functionality',
+      'http://clipart-library.com/data_images/125152.png'
+    )
+  ];
   constructor() { }
 
   ngOnInit() {
